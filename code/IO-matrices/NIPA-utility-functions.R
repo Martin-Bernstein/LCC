72b5dabdb2109c07b24568810325b990279177ff
Martin Bernstein <martinmbernstein@gmail.com>
2025-08-16 14:45:46 -0400

    add IO matrix

--- code/IO-matrices/NIPA-utility-functions.R
+++ code/IO-matrices/NIPA-utility-functions.R
@@ -8,6 +8,7 @@ library(tidyr)
 library(RColorBrewer)
 library(plotly)
 library(zoo)
+library(docstring)
 
 backg<-theme(
   plot.background = element_rect(fill = "transparent", colour = NA),
@@ -17,99 +18,113 @@ backg<-theme(
   legend.key = element_rect(fill = "transparent", colour = NA)
 )
 
-clean_NIPA <- function(dh, years,colname,use_deflator = F){
-  names(dh)[1] <- 'row'
-  names(dh)[2] <- 'industry'
-  # Convert these years to character just once
-  year_cols <- as.character(years)
-  
-  #NIPA industries:
-  key <- fread(file.path('data','NIPA','GDP-by-industry','NIPA industries_gross output.csv'))
-  key[, industry := trimws(industry)]
-  
-  #given NIPA data:
-  dh[, industry := trimws(industry)]
-  #remove all digits:
-  dh[, industry := trimws(gsub("\\d", "", industry))]
-  
-  #Add sector key
-  dh <- merge(dh, key, by = 'industry')
-  dh <- dh[, c('row', 'id', 'level', 'parent', 'industry', all_of(year_cols)), with = FALSE]  # <- key change
-  setorder(dh, id)
-  
-  #Remove addenda
-  dh <- dh[level >= 0]
-  
-  dh[, (year_cols) := lapply(.SD, as.numeric), .SDcols = year_cols]
-  
-  #Long format
-  dh <- pivot_longer(dh, cols = year_cols, names_to = 'year', values_to = colname) %>% setDT()
-  setorder(dh, year, row)
-  dh[, c(colname, 'year') := .(as.numeric(get(colname)), as.numeric(year))]
-  
-  #Use price deflator
-  if(use_deflator == T){
-   pce <- fread(file.path('data','FRED','PCECTPI.csv')) 
-   pce[,year := year(observation_date)]
-   pce <- pce[, .(PCECTPI_an = mean(PCECTPI)),by=.(year)]
-   dh[,pi := pce[.SD,on=.(year),x.PCECTPI_an]]
-   dh[, c(colname) := .(get(colname) / (pi / 100))]
-   dh[,pi := NULL]
+
+load_io_table <- function(year){
+  #' Load IO table for a given year
+  if (year <= 1996) {
+    m <- suppressMessages(read_excel(
+      file.path(
+        "data", "NIPA",
+        "IO tables", "IO_1963-1996",
+        "IOUse_Before_Redefinitions_PRO_1963-1996_Summary.xlsx"
+      ),
+      sheet = as.character(year)
+    ))
+    colnames(m) <- m[5, ]
+    colnames(m)[1:2] <- c("IOCode", "Name")
+    colcodes <- m[6, 3:ncol(m)]
+  } else {
+    m <- suppressMessages(read_excel(
+      file.path(
+        "data", "NIPA", "IO tables", "IO_1997-2023",
+        "IOUse_Before_Redefinitions_PRO_1997-2023_Summary.xlsx"
+      ),
+      sheet = as.character(year)
+    ))
+    colnames(m) <- m[6, ]
+    colcodes <- m[5, ]
   }
-  
-  dh
+  return(list(m, colcodes))
 }
 
 
-#Function for generic NIPA data. Given y1 and y2 year ranges (y1 <= y2)
-join_NIPA <- function(dh,d,y1,y2,colname){
-  dh <- clean_NIPA(dh,y1,colname)
-  d <- clean_NIPA(d,y2,colname)
-  
-  #Avoid duplicates: delete from the older dataset any years included in the newer one.
-  dh <- dh[year %in% y1[!y1 %in% y2]]
-  
-  #Historical and modern data:
-  d <- rbind(dh,d)
-  d[,row:=NULL]
-  
-  
-  #Non-missing data:
-  d <- d[!is.na(get(colname))]
-  
-  # Sector children
-  d <- make_children(d)
-  
-  setorder(d,year,id) 
-  
-  d
-}
 
-
-verify_crosswalk <- function(cw){
-  ind <- fread(file.path("data", "codes and crosswalks",
-                         "IND1990_codes.csv"))
-  ind <- ind[Code != 0]
-  ind[, in_cw := Code %in% cw$IND1990_code]
-  check_cw_problems(cw, ind)
+build_matrix <- function(m, colcodes, cw, year){
+  #' Function to build clean IO matrix
+  #' 
+  #' @param m data table. The raw IO matrix
+  #' @param colcodes data table. Key for column (industry) IO codes
+  #' @param cw data table. Crosswalk for our NNA codes
+  #' @param year int. The year being assembled
+  #' 
+  #' @returns data table. The IO matrix in clean long format
   
-  io <- fread(file.path("data", "codes and crosswalks",
-                        "IO_pre1997_codes.csv"))
-  io[, in_cw := IO_pre1997_code %in% cw$IO_pre1997_code]
-  check_cw_problems(cw, io)
-  
-  io <- fread(file.path("data", "codes and crosswalks",
-                        "IO_post1997_codes.csv"))
-  io[, in_cw := IO_post1997_code %in% cw$IO_post1997_code]
-  check_cw_problems(cw, io)
-  
-  
-}
-
-check_cw_problems <- function(cw, ind, codename){
-  problems <- nrow(ind[in_cw == F])
-  if(problems > 0){
-    print(paste0("Warning: Unassigned ", codename, " codes"))
+  #Codes for commodities (rows)
+  key <- m[, .(IOCode, Name)]
+  if(year <= 1996){
+    usecw <- unique(cw[, .(IO_pre1997_code, NNA_code)])
+    setnames(usecw, old = "IO_pre1997_code", new = "IOCode")
+  }else{
+    usecw <- unique(cw[, .(IO_post1997_code, NNA_code)])
+    setnames(usecw, old = "IO_post1997_code", new = "IOCode")
   }
-  rm(ind, problems) 
+  key[, NNA_code := usecw[.SD, on = .(IOCode), x.NNA_code]]
+  
+  
+  #Pivot and add commmodity NNA codes
+  dt <- setDT(pivot_longer(m, cols = 3:ncol(m),
+                           names_to = "industry", values_to = "value"))
+  dt[, code_commodity := unique(key[, .(Name, NNA_code)])
+     [.SD, on = .(Name), x.NNA_code]]
+  #Add industry codes
+  dt[, code_industry := colcodes[.SD, on = .(industry), x.NNA_code]]
+  
+  suppressWarnings(dt[, value := as.numeric(value)]) # Converts ".." to NA.
+  dt[is.na(value), value := 0]
+  
+  setnames(dt, old = "Name", new = "commodity")
+  #Total industry output and value added (per unique NNA_codes):
+  totals <- dt[commodity %in% c("Total Industry Output", "Total Value Added",
+                                "Total Intermediate"),
+               .(code_industry, value, commodity)]
+  totals <- totals[, .(value = sum(value)), 
+                   by = .(code_industry, commodity)]
+  
+  #Aggregate by NNA code, since some are duplicated:
+  dt <- dt[, .(value = sum(value)), by = .(code_industry, code_commodity)]
+  dt[, year := year]
+  dt <- dt[!is.na(code_commodity)] #Removes all aggregate commodities
+  dt <- dt[!is.na(code_industry)] #Removes all final use industries
+  
+  #Add names and totals
+  NNAcodes <- unique(cw[, .(NNA_code, NNA_industry)])
+  dt[, industry := NNAcodes[.SD, on = .(NNA_code = code_industry), x.NNA_industry]]
+  dt[, commodity := NNAcodes[.SD, on = .(NNA_code = code_commodity), x.NNA_industry]]
+  
+  dt[, industry_total_output := totals[commodity == "Total Industry Output"]
+     [.SD,on = .(code_industry), x.value]]
+  dt[, industry_value_added := totals[commodity == "Total Value Added"]
+     [.SD,on = .(code_industry), x.value]]
+  dt[, industry_intermediates := totals[commodity == "Total Intermediate"]
+     [.SD,on = .(code_industry), x.value]]
+  
+  #Format
+  dt <- dt[, .(year, code_industry, industry, code_commodity, commodity, 
+               value, industry_value_added, industry_total_output, industry_intermediates)]
+  setorder(dt,code_industry,code_commodity)
+  
+  
+  #Shares
+  dt[,share_in_output := value / industry_total_output]
+  
+  #Test that sums are correct:
+  dt[, test := sum(value) / industry_intermediates, by=.(code_industry)]
+  resid <- max(abs(dt$test - 1))
+  if(resid > 0.02){
+    print(paste0("Warning: error of ", round(resid, digits = 5),
+                 " in year ",year))
+  }
+  dt[, c("test", "industry_intermediates") := NULL]
+  
+  return(dt)
 }
--- code/IO-matrices/make_IO_tables.R
+++ code/IO-matrices/make_IO_tables.R
@@ -15,36 +15,15 @@ verify_crosswalk(cw) #Prints if error, otherwise does nothing.
 
 
 years <- 1963:2023
-#i<-38
 for (i in seq_along(years)) {
-  # Read data (depending on year)
+  
   year <- years[i]
   print(paste0("Building matrix for ", year))
-  if (year <= 1996) {
-    m <- read_excel(
-      file.path(
-        "data", "NIPA",
-        "IO tables", "IO_1963-1996",
-        "IOUse_Before_Redefinitions_PRO_1963-1996_Summary.xlsx"
-      ),
-      sheet = as.character(year)
-    )
-    colnames(m) <- m[5, ]
-    colnames(m)[1:2] <- c("IOCode", "Name")
-    colcodes <- m[6, 3:ncol(m)]
-  } else {
-    m <- read_excel(
-      file.path(
-        "data", "NIPA", "IO tables", "IO_1997-2023",
-        "IOUse_Before_Redefinitions_PRO_1997-2023_Summary.xlsx"
-      ),
-      sheet = as.character(year)
-    )
-    colnames(m) <- m[6, ]
-    colcodes <- m[5, ]
-  }
   
-  
+  # Read IO data
+  res <- load_io_table(year)
+  m <- res[[1]]
+  colcodes <- res[[2]]
   
   # Extract codes for industries (columns) and code with NNA codes
   colcodes <- pivot_longer(colcodes,
@@ -53,6 +32,7 @@ for (i in seq_along(years)) {
   setDT(colcodes)
   colcodes <- colcodes[!is.na(ind_code)]
   
+  #Apply NNA codes
   if(year <= 1996){
     usecw <- unique(cw[, .(IO_pre1997_code, NNA_code)])
     colcodes[, NNA_code := 
@@ -63,90 +43,22 @@ for (i in seq_along(years)) {
                usecw[.SD, on = .(IO_post1997_code = ind_code), x.NNA_code]]
   }
   
-  
-  
   # Clean
   m <- m[7:nrow(m), ]
   setDT(m)
+  
   # Use this to build matrix
-  res <- build_matrix(m, colcodes, year)
-  dt <- res[[1]]
-  agg <- res[[2]]
+  dt <- build_matrix(m, colcodes, cw, year)
+  
+  # Combine into one dataset
   if(i == 1){
     all_data <- dt
-    all_agg <- agg
   }else{
     all_data <- rbind(all_data, dt)
-    all_agg <- rbind(all_agg, agg)
   }
 }
 
-#Function to build clean IO matrix and aggregates
-#CURRENTLY ONLY WORKS post 1997: need to fix this for pre 1996.
-build_matrix <- function(m, colcodes, year){
-  #Format long
-  key <- m[!is.na(IOCode) & !is.na(Name), .(IOCode, Name)]
-  key[, NNA_code := colcodes[.SD, on = .(ind_code = IOCode), x.NNA_code]]
-  
-  #Make sure the rows and columns agree
-  unqcols <- sort(unique(colcodes[!is.na(NNA_code)]$ind_code))
-  unqrows <- sort(unique(key[!is.na(NNA_code)]$IOCode))
-  if (!sum(unqcols == unqrows) == length(unqrows)){
-    print("WARNING: Coded rows and columns of IO table don't agree")
-  }
-  
-  
-  
-  dt <- setDT(pivot_longer(m, cols = 3:(ncol(m)-2),
-                           names_to = "industry", values_to = "value"))
-  dt[, year := year]
-  dt[,c('Total Final Uses (GDP)','Total Commodity Output') := NULL]
-  
-  #Clean, get key
-  dt[,code_industry := colcodes[.SD,on = .(industry), x.ind_code]]
-  
-  #Remove final uses
-  dt <- dt[!substr(code_industry,1,1)=="F"]
-  convert <- c("value")
-  dt[,(convert) := lapply(.SD, as.numeric),.SDcols = convert]
-  
-  #Industry aggregates:
-  totals <- dt[commodity == "Total Industry Output", .(industry, value)]
-  dt[, total_industry_output := totals[.SD, on = .(industry), x.value]]
-  
-  #For post 1997
-  aggs <- dt[commodity == 'Total Intermediate' | 
-               code_commodity %in% c("V001", "V002", "V003")]
-  
-  ##NEED TO MAKE ANOTHER for pre 1997
-  ##HERE## different codes
-  
-  aggs[,code_commodity := NULL]
-  setnames(aggs, old = "commodity", new = "item")
-  aggs <- aggs[, .(code_industry, industry, item, value, total_industry_output)]
-  
-  #Format - remove aggregates
-  dt <- dt[!is.na(code_commodity) & !is.na(commodity)] #No code in post-1996
-  dt <- dt[!code_industry %in% c("T001", "T004", "T007")] #These are agg codes in pre-1996
-  dt <- dt[!code_commodity %in% c("T005", "T006", "T008")]
-  
-  
-  dt <- dt[, .(year, code_industry, industry, code_commodity, commodity, 
-               value, total_industry_output)]
-  
-  
-  #Shares
-  setorder(dt,code_industry,code_commodity)
-  dt[is.na(value), value := 0]
-  dt[,share := value / total_industry_output]
-  
-  #Test that sums are correct:
-  dt[, test := sum(value) / total_industry_output, by=.(code_industry)]
-  resid <- max(abs(dt$test - 1))
-  if(resid > 1e-3){
-    print(paste0("Warning: error of ", round(resid, digits = 5),
-                 " in year ",year))
-  }
-  
-  return(list(dt, aggs))
-}
+
+write.csv(all_data, file = file.path("data", "constructed data", "IO-matrices",
+                                     "IO_matrices_long.csv"),
+          row.names = F)
